cebffbcdb258f62344433a749ea02e39d1ce2159
lucarafe <luc.bonin0903@gmail.com>
2023-12-26 13:03:20 +0100

    modif footer - text whithe

--- components/Footer.js
+++ components/Footer.js
@@ -11,12 +11,12 @@ export default function Footer() {
                 <p className={styles.p}>Téléphone : +33 7 71 87 09 03</p>
                 <p className={styles.p}>Email : luc.bonin0903@gmail.com</p>
             </div>
-            <div>
+            <div className={styles.white}>
                 <h4 className={styles.h4}>Suivez-moi</h4>
                 <ul className={styles.ul}>
-                    <li className={styles.li}><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x text-white"></i></a></li>
-                    <li className={styles.li}><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x text-white"></i></a></li>
-                    <li className={styles.li}><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x text-white"></i></a></li>
+                    <li className={styles.li}><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x"></i></a></li>
+                    <li className={styles.li}><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x"></i></a></li>
+                    <li className={styles.li}><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x"></i></a></li>
                 </ul>
             </div>
         </footer>
