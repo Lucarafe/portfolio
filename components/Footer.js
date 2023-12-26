f0768f923bb6fbbd9837407d711fa1e7fdd1dd08
lucarafe <luc.bonin0903@gmail.com>
2023-12-26 11:42:43 +0100

    modif footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,25 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
-      </footer>
+        <footer className="bg-dark text-white py-4">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h4>Contact</h4>
+                        <p>Téléphone : +33 7 71 87 09 03</p>
+                        <p>Email : luc.bonin0903@gmail.com</p>
+                    </div>
+                    <div className="col-md-6">
+                        <h4>Suivez-moi</h4>
+                        <ul className="list-inline">
+                            <li className="list-inline-item"><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x text-white"></i></a></li>
+                            <li className="list-inline-item"><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x text-white"></i></a></li>
+                            <li className="list-inline-item"><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x text-white"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </>
   )
 }
