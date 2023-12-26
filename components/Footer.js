1d6d631ceb9908dfcb0dabe15c971fd8b18f37d6
lucarafe <luc.bonin0903@gmail.com>
2023-12-26 12:04:04 +0100

    modif footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-        <footer className="container">
+        <footer className={styles.footer}>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
             <div>
@@ -14,9 +14,9 @@ export default function Footer() {
             <div>
                 <h4>Suivez-moi</h4>
                 <ul>
-                    <li><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x text-white"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x text-white"></i></a></li>
-                    <li><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x text-white"></i></a></li>
+                    <li><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x"></i></a></li>
+                    <li><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x"></i></a></li>
                 </ul>
             </div>
 
