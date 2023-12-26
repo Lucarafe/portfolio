2b6217031bde963b3da65f8f6a83bb0d0e4860fd
lucarafe <luc.bonin0903@gmail.com>
2023-12-26 11:59:23 +0100

    modif footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,23 +3,22 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-        <footer className="bg-dark text-white py-4">
-            bootstrap
+        <footer className="container">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
+            <div>
+                <div>
+                    <div>
                         <h4>Contact</h4>
                         <p>Téléphone : +33 7 71 87 09 03</p>
                         <p>Email : luc.bonin0903@gmail.com</p>
                     </div>
-                    <div className="col-md-6">
+                    <div>
                         <h4>Suivez-moi</h4>
-                        <ul className="list-inline">
-                            <li className="list-inline-item"><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x text-white"></i></a></li>
-                            <li className="list-inline-item"><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x text-white"></i></a></li>
-                            <li className="list-inline-item"><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x text-white"></i></a></li>
+                        <ul>
+                            <li><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x text-white"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x text-white"></i></a></li>
+                            <li><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x text-white"></i></a></li>
                         </ul>
                     </div>
                 </div>
