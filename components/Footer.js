4c5ce91ac1dec4f0f94003b00670496bf82e1b11
lucarafe <luc.bonin0903@gmail.com>
2023-12-26 12:00:34 +0100

    modif footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,21 +7,17 @@ export default function Footer() {
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
             <div>
-                <div>
-                    <div>
-                        <h4>Contact</h4>
-                        <p>Téléphone : +33 7 71 87 09 03</p>
-                        <p>Email : luc.bonin0903@gmail.com</p>
-                    </div>
-                    <div>
-                        <h4>Suivez-moi</h4>
-                        <ul>
-                            <li><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x text-white"></i></a></li>
-                            <li><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x text-white"></i></a></li>
-                            <li><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x text-white"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
+                <h4>Contact</h4>
+                <p>Téléphone : +33 7 71 87 09 03</p>
+                <p>Email : luc.bonin0903@gmail.com</p>
+            </div>
+            <div>
+                <h4>Suivez-moi</h4>
+                <ul>
+                    <li><a href="https://www.instagram.com/lucarafe/"><i className="fab fa-instagram fa-2x text-white"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/lucas-bonin-334836234/"><i className="fab fa-linkedin fa-2x text-white"></i></a></li>
+                    <li><a href="https://github.com/Lucarafe"><i className="fab fa-github fa-2x text-white"></i></a></li>
+                </ul>
             </div>
 
         </footer>
