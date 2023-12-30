1bfe11016a049c61e04e011863b00213a6730452
lucarafe <luc.bonin0903@gmail.com>
2023-12-30 19:12:26 +0100

    script js

---
+++ components/Script.js
@@ -0,0 +1,27 @@
+// Récupérer tous les boutons radio
+
+const navLinks = document.querySelectorAll('.baniere');
+export default function Script() {
+
+    return () => {
+
+        navLinks.forEach(link => {
+            link.addEventListener('click', handleNavLinkClick);
+        });
+    }
+}
+
+function handleNavLinkClick(event) {
+    // Empêcher le comportement par défaut du lien
+    event.preventDefault();
+
+    // Réinitialiser les noms de classe pour tous les liens
+    navLinks.forEach(link => {
+        link.classList.remove('active', 'text-black');
+        link.classList.add('text-white-50');
+    });
+
+    // Ajouter les noms de classe au lien cliqué
+    this.classList.remove('text-white-50');
+    this.classList.add('active', 'text-black');
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Script from "@components/script";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
         <Header />
       <Footer />
+        <Script />
     </div>
   )
 }
